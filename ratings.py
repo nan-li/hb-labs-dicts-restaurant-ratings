3f539119b62bcd7f5b0e3ee9467ccf24ff4ac613
Nan Li <nanftw@gmail.com>
2021-01-11 17:50:22 -0800

    Prompt user for restaurant rating
    Co-authored by: Janaki Ranpura <automatic.janaki@gmail>

--- ratings.py
+++ ratings.py
@@ -15,6 +15,11 @@ def restaurant_ratings(filename):
         restaurant_name, score = restaurant_info
 # define dict = {'Ministry Munchies': 1, 'The Porcupine': 5}        
         restaurantnames_and_scores[restaurant_name] = score
+#input from user
+    print("What restaurant would you like to rate?")
+    user_restaurant = input("> ")
+    user_score = input("Please provide restaurant score: ")
+    
 # alphabetize the list        
     sorted_list = sorted(restaurantnames_and_scores)
 # for i in keysortedlist    
@@ -22,6 +27,15 @@ def restaurant_ratings(filename):
     for restaurant_name in sorted_list:
         print(f'{restaurant_name} is rated at {restaurantnames_and_scores[restaurant_name]}.')
 
-        
+
+# The program should:
+#     Prompt the user for a restaurant name
+#     Prompt the user for a restaurant score
+#     Store the new restaurant/rating in the dictionary
+#     Print all of the ratings in alphabetical order (including the new one, of course)
+
+
+
+
 
 restaurant_ratings("scores.txt")
